01dcc96b86879f774a695f64e26eaf23bd0733eb
Abakshin-bavitsk Gleb <abakshin-bavitskgg@bcs.ru>
2022-12-04 18:36:32 +0500

    - change icon;

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'CV',
       theme: ThemeData(primarySwatch: Colors.blueGrey),
       home: const MyHomePage(title: 'Abakshin Gleb', job: 'Java Developer'),
     );
