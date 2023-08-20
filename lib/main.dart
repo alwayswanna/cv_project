616432ab2da3095de274516a2429620b8ef3466e
Gleb <a.gleb@yahoo.com>
2023-08-20 13:57:52 +0500

    + refactoring page;

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'dart:convert';
 
-import 'package:animate_gradient/animate_gradient.dart';
 import 'package:cv_project/models/personal_data.dart';
-import 'package:cv_project/views/fullscreen.dart';
+import 'package:cv_project/views/full_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,16 +16,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'CV',
       theme: ThemeData(primarySwatch: Colors.blueGrey),
-      home: const MyHomePage(title: 'Abakshin Gleb', job: 'Java Developer'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title, required this.job});
-
-  final String title;
-  final String job;
+  const MyHomePage({super.key});
 
   @override
   State<StatefulWidget> createState() {
@@ -46,103 +42,56 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: PreferredSize(
-            preferredSize: const Size.fromHeight(120.0),
+            preferredSize: const Size.fromHeight(10.0),
             child: AppBar(
-              backgroundColor: Colors.black,
-              toolbarHeight: 120.0,
+              backgroundColor: Colors.indigo,
               automaticallyImplyLeading: false,
-              flexibleSpace: Container(),
               centerTitle: true,
               elevation: 0,
-              title: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Row(
-                    children: [
-                      Text(
-                        widget.title,
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 50,
-                          fontFamily: "Inconsolata",
-                        ),
-                      )
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        widget.job,
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontFamily: "Inconsolata",
-                        ),
-                      )
-                    ],
-                  )
-                ],
-              ),
             )),
-        body: SingleChildScrollView(child: LayoutBuilder(
-          builder: (BuildContext context, BoxConstraints constraints) {
-            if (constraints.maxWidth > 600) {
-              return AnimateGradient(
-                  primaryBegin: Alignment.topLeft,
-                  primaryEnd: Alignment.bottomLeft,
-                  secondaryBegin: Alignment.bottomLeft,
-                  secondaryEnd: Alignment.topRight,
-                  primaryColors: const [
-                    Colors.green,
-                    Colors.greenAccent,
-                    Colors.white,
-                  ],
-                  secondaryColors: const [
-                    Colors.white,
-                    Colors.blueAccent,
-                    Colors.blue,
-                  ],
-                  child: FutureBuilder(
-                      future: DefaultAssetBundle.of(context)
-                          .loadString('assets/data/main-data.json'),
-                      builder: (context, snapshot) {
-                        if (!snapshot.hasData) {
-                          return const CircularProgressIndicator();
-                        } else {
-                          return FullScreenWidget()
-                              .fullScreenView(context, PersonalData.fromJson(jsonDecode(snapshot.data!)), 1.0);
-                        }
-                      }));
-            } else {
-              return AnimateGradient(
-                  primaryBegin: Alignment.topLeft,
-                  primaryEnd: Alignment.bottomLeft,
-                  secondaryBegin: Alignment.bottomLeft,
-                  secondaryEnd: Alignment.topRight,
-                  primaryColors: const [
-                    Colors.green,
-                    Colors.greenAccent,
-                    Colors.white,
-                  ],
-                  secondaryColors: const [
-                    Colors.white,
-                    Colors.blueAccent,
-                    Colors.blue,
-                  ],
-                  child: FutureBuilder(
-                      future: DefaultAssetBundle.of(context)
-                          .loadString('assets/data/main-data.json'),
-                      builder: (context, snapshot) {
-                        if (!snapshot.hasData) {
-                          return const CircularProgressIndicator();
-                        } else {
-                          return FullScreenWidget()
-                              .fullScreenView(context, PersonalData.fromJson(jsonDecode(snapshot.data!)), 0.5);
-                        }
-                      }));
-            }
-          },
-        )));
+        body: Align(
+            alignment: Alignment.center,
+            child: Container(
+              padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+              decoration: const BoxDecoration(
+                  border: Border.symmetric(
+                      vertical: BorderSide(color: Colors.black),
+                      horizontal: BorderSide.none)),
+              child: SizedBox(
+                  width: 800,
+                  height: 1200,
+                  child: SingleChildScrollView(child: LayoutBuilder(builder:
+                      (BuildContext context, BoxConstraints constraints) {
+                    if (constraints.maxWidth > 600) {
+                      return FutureBuilder(
+                          future: DefaultAssetBundle.of(context)
+                              .loadString('assets/data/main-data.json'),
+                          builder: (context, snapshot) {
+                            if (!snapshot.hasData) {
+                              return const CircularProgressIndicator();
+                            } else {
+                              return FullScreenWidget().fullScreenView(
+                                  PersonalData.fromJson(
+                                      jsonDecode(snapshot.data!)),
+                                  1.0);
+                            }
+                          });
+                    } else {
+                      return FutureBuilder(
+                          future: DefaultAssetBundle.of(context)
+                              .loadString('assets/data/main-data.json'),
+                          builder: (context, snapshot) {
+                            if (!snapshot.hasData) {
+                              return const CircularProgressIndicator();
+                            } else {
+                              return FullScreenWidget().fullScreenView(
+                                  PersonalData.fromJson(
+                                      jsonDecode(snapshot.data!)),
+                                  0.5);
+                            }
+                          });
+                    }
+                  }))),
+            )));
   }
 }
--- lib/models/personal_data.dart
+++ lib/models/personal_data.dart
@@ -1,22 +1,21 @@
 class PersonalData {
   final String personalData;
-  final String yearOfExperience;
   final Map<String, int> skills;
   final List<dynamic> workExperience;
   final SocialLinks socialLinks;
+  final MainInfo mainInfo;
 
-  PersonalData({
-        required this.personalData,
-        required this.yearOfExperience,
-        required this.skills,
-        required this.workExperience,
-        required this.socialLinks
-  });
+  PersonalData(
+      {required this.personalData,
+      required this.skills,
+      required this.workExperience,
+      required this.socialLinks,
+      required this.mainInfo});
 
   PersonalData.fromJson(Map<String, dynamic> json)
       : personalData = json['personal-data'],
-        yearOfExperience = json['year-of-experience'],
         skills = Map.castFrom(json['skills']),
+        mainInfo = MainInfo.fromJson(json['mainInfo']),
         socialLinks = SocialLinks.fromJson(json['social-links']),
         workExperience = List<dynamic>.from(json['work-experience'])
             .map((key) => WorkExperience.fromJson(key))
@@ -25,22 +24,18 @@ class PersonalData {
 
 class WorkExperience {
   final String company;
-  final String responsibilities;
   final String period;
   final String job;
   final String skills;
 
-  WorkExperience({
-      required this.company,
-      required this.responsibilities,
+  WorkExperience(
+      {required this.company,
       required this.period,
       required this.job,
-      required this.skills
-  });
+      required this.skills});
 
   WorkExperience.fromJson(Map<String, dynamic> json)
       : company = json['company'],
-        responsibilities = json['responsibilities'],
         period = json['period'],
         job = json['job'],
         skills = json['skills'];
@@ -50,15 +45,46 @@ class SocialLinks {
   final String facebook;
   final String linkedIn;
   final String habrCareer;
+  final String github;
+  final String email;
 
   SocialLinks({
     required this.facebook,
     required this.linkedIn,
     required this.habrCareer,
+    required this.github,
+    required this.email
   });
 
   SocialLinks.fromJson(Map<String, dynamic> json)
       : facebook = json['facebook'],
         linkedIn = json['linkedIn'],
-        habrCareer = json['habrCareer'];
+        habrCareer = json['habrCareer'],
+        github = json['github'],
+        email = json['email'];
+}
+
+class MainInfo {
+  final String firstName;
+  final String lastName;
+  final String birthDate;
+  final String currentLocation;
+  final String experience;
+  final String addition;
+
+  MainInfo(
+      {required this.birthDate,
+      required this.firstName,
+      required this.lastName,
+      required this.currentLocation,
+      required this.experience,
+      required this.addition});
+
+  MainInfo.fromJson(Map<String, dynamic> json)
+      : firstName = json['firstName'],
+        lastName = json['lastName'],
+        birthDate = json['birthDate'],
+        currentLocation = json['currentLocation'],
+        experience = json['experience'],
+        addition = json['addition'];
 }
